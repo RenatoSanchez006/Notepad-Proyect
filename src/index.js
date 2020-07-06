2405a2d1ff23988e4796a7872da97916e1631f8d
Renato Sánchez <renato@emissary.mx>
2020-07-06 16:41:20 -0500

    Creating my first components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Banner(props) {
+    return <p>This is the first {props.name} in {props.day}.</p>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function BigBanner() {
+    return(
+        <div>
+            <Banner name='session' day="week"></Banner>
+            <Banner name='class' day="month"></Banner>
+            <Banner name='course'></Banner>
+        </div>
+    );
+}
+
+function tick () {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <BigBanner/>
+        </div>
+    
+    );
+    
+    ReactDOM.render(
+        element, document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
